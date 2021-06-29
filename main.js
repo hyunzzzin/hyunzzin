753ad7e1a76f42c470faedc12ff5c5741f4b7e00
ahnhyunjin1 <a01020813765@gmail.com>
2021-06-29 17:24:01 +0900

    ECES_1

--- main.js
+++
@@ -1,9 +0,0 @@
-const togglebtn = document.querySelector('.navbar__togglebtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-
-
-togglebtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-});
